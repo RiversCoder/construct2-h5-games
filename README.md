b25093f97e25386fd52438c88353f2ef924be4c1
RiversCoder <928091232@qq.com>
2018-05-17 15:02:16 +0800

    cgame

---
+++ README.md
@@ -0,0 +1,6 @@
+## Use construct 2 create many h5 games.
+========
+
+### Hit Plane H5 Games
+
+### Tappy Plane H5 Games
---
+++ Tappy_Plane/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1526539865,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/groundrock-sheet0.png",
+		"images/planegreen-sheet0.png",
+		"images/planegreen-sheet1.png",
+		"images/planegreen-sheet2.png",
+		"images/rockice-sheet0.png",
+		"images/rockicedown-sheet0.png",
+		"images/pufflarge-sheet0.png",
+		"images/stargold-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
